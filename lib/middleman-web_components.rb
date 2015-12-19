d41bd1f3cd30650d84170a5a6fe6abef73f3325f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-12-20 04:25:16 +0900

    Remove unneccessary variable

--- lib/middleman-web_components.rb
+++ lib/middleman-web_components.rb
@@ -26,8 +26,7 @@ class Middleman::WebComponents < ::Middleman::Extension
     resources.collect do |resource|
       next resource if resource.ignored?
       next resource unless resource.path.start_with? options.directory
-      command = "vulcanize #{resource.source_file}"
-      content = `#{command}`
+      command = `vulcanize #{resource.source_file}`
       resource = Middleman::Sitemap::StringResource.new(app.sitemap, resource.path, content)
       resource.destination_path = Pathname(resource.path).sub_ext(options.suffix).to_path unless resource.ext == options.suffix
       resource
